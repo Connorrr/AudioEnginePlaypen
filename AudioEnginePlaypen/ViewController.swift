ab9b905440ce041194d06771d8d7e5b4a9588991
Connorrr <connor.reid87@gmail.com>
2016-06-15 15:29:40 +1000

    Initial Commit

---
+++ AudioEnginePlaypen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioEnginePlaypen
+//
+//  Created by Connor Reid on 15/06/2016.
+//  Copyright © 2016 Connor Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
