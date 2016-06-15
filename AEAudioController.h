eaee9328f27349b1c49e45cfe739441a0505053e
Connorrr <connor.reid87@gmail.com>
2016-06-15 15:54:03 +1000

    Created empty AudioController Class.  Linked with bridging header.   Created all encompassing workspace.  Linked all search headers to TAE2 lib.

---
+++ AEAudioController.h
@@ -0,0 +1,14 @@
+//
+//  AEAudioController.h
+//  AudioEnginePlaypen
+//
+//  Created by Connor Reid on 15/06/2016.
+//  Copyright © 2016 Connor Reid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <TheAmazingAudioEngine/TheAmazingAudioEngine.h>
+
+@interface AEAudioController : NSObject
+
+@end
---
+++ AEAudioController.m
@@ -0,0 +1,13 @@
+//
+//  AEAudioController.m
+//  AudioEnginePlaypen
+//
+//  Created by Connor Reid on 15/06/2016.
+//  Copyright © 2016 Connor Reid. All rights reserved.
+//
+
+#import "AEAudioController.h"
+
+@implementation AEAudioController
+
+@end
